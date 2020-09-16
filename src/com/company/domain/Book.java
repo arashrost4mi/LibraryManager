f7c9d9a903572ab2448dc9d49fffb5c6b4c48496
arashrost4mi <arashrost4mi@gmail.com>
2020-09-17 03:23:42 +0430

    initial commit: Book and Client classes done

--- src/com/company/domain/Book.java
+++ src/com/company/domain/Book.java
@@ -1,4 +1,62 @@
 package com.company.domain;
 
 public class Book {
+
+    int id;
+
+    String name;
+
+    String writer;
+
+    boolean borrowed;
+
+    int clientId;
+
+    public Book(int id, String name, String writer, boolean borrowed, int clientId) {
+        this.id = id;
+        this.name = name;
+        this.writer = writer;
+        this.borrowed = borrowed;
+        this.clientId = clientId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
 }
