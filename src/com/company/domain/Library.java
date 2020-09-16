4b70522789f5c7f63fc58e62961270abc6662b4c
NimaSeyfi <nima.different76@gmail.com>
2020-09-17 03:22:18 +0430

    Library domain done

--- src/com/company/domain/Library.java
+++ src/com/company/domain/Library.java
@@ -1,4 +1,43 @@
 package com.company.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+    private int id;
+    private String name;
+    List<Book> books = new ArrayList<Book>();
+    List<Client> clients = new ArrayList<Client>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
 }
